0d34bad512e8c5a32e4db2f243d5a58918df035a
kinnrai <me@kinnrai.com>
2025-12-01 10:55:55 +0800

    Update .config/hammerspoon/init.lua

--- home/dot_config/hammerspoon/init.lua
+++ home/dot_config/hammerspoon/init.lua
@@ -1,10 +1,24 @@
 -- Use [Option+Q] to move mouse between different screens
 hs.hotkey.bind({'option'}, 'Q', function()
+    -- get current screen and calculate the center point of the next screen
     local screen = hs.mouse.getCurrentScreen()
     local nextScreen = screen:next()
     local rect = nextScreen:fullFrame()
     local center = hs.geometry.rectMidPoint(rect)
+    -- move mouse cursor to the center of the next screen
     hs.mouse.absolutePosition(center)
+    -- iterate through windows to find which one is under the cursor
+    local windows = hs.window.orderedWindows()
+    for _, win in ipairs(windows) do
+        if win:isVisible() and win:isStandard() and win:screen() == nextScreen then
+            local f = win:frame()
+            if center.x >= f.x and center.x <= (f.x + f.w) and
+               center.y >= f.y and center.y <= (f.y + f.h) then
+                win:focus()
+                break
+            end
+        end
+    end
 end)
 
 -- Use [Option+W] to move window between different screens
